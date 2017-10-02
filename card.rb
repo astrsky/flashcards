8e47ca2e31d1ecab81ec552261b599581036a02b
Ilya Kogan <astrsky1@gmail.com>
2017-10-02 19:21:23 +0300

    Add files via upload

--- card.rb
+++ card.rb
@@ -1,9 +1,9 @@
 class Card < ApplicationRecord
-  validates :original_text, :translated_text, :review_date , presence: true
+  validates :original_text, :translated_text, presence: true #тут еще будет equal 
   validates :original_text, uniqueness: {message: "Word already exist"}
   validates :translated_text, uniqueness: {message: "Word already exist"}
 
-  before_create :set_review_date
+before_create :set_review_date
 
 def set_review_date
   self.review_date = 3.days.since
