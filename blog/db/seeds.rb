3a876c513a16b048690ffb44e728181c49e15a71
Ilya Kogan <astrsky1@gmail.com>
2017-10-09 15:59:10 +0300

    Add files via upload

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -9,10 +9,13 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://wordsteps.com/vocabulary/words/46729/"))
-row = doc.css('#WordsTable tbody tr').each do |row|
-  original_text = row.css('td.static.word').text
-  translated_text = row.css('td.static.trans').text
+doc = Nokogiri::HTML(open("https://www.memrise.com/course/920/5000-german-words-top-87/"))
+row = doc.css('div.span9 .levels a.level').map { |a| a.attributes['href'] }.each do |row| 
+page = Nokogiri::HTML(open("row"))
+link = page.css('.central.column .things.clearfix .thing.text-text').each do |link|
+  original_text = link.css(' div.col_a.col.text').text
+  translated_text = link.css('div.col_b.col.text').text
+  puts "#{original_text}\t#{translated_text}"
 
   Card.create(original_text: original_text, translated_text: translated_text)
-end 
+end
