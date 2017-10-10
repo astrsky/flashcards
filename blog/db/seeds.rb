020f1c3477183f51bb123ceff90903cad135f433
Ilya Kogan <astrsky1@gmail.com>
2017-10-10 12:47:05 +0300

    Add files via upload

--- blog/db/seeds.rb
+++ blog/db/seeds.rb
@@ -10,12 +10,13 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://www.memrise.com/course/920/5000-german-words-top-87/"))
-row = doc.css('div.span9 .levels a.level').map { |a| a.attributes['href'] }.each do |row| 
-page = Nokogiri::HTML(open("row"))
-link = page.css('.central.column .things.clearfix .thing.text-text').each do |link|
-  original_text = link.css(' div.col_a.col.text').text
-  translated_text = link.css('div.col_b.col.text').text
-  puts "#{original_text}\t#{translated_text}"
+doc.css('div.span9 .levels a.level').map { |a| a.attributes['href'].value }.each do |row| 
+  page = Nokogiri::HTML(open("https://www.memrise.com/" + row))
+  page.css('div.container.container-main .central-column .things.clearfix .thing.text-text').each do |link|
+    original_text = link.css('div.col_a.col.text').text
+    translated_text = link.css('div.col_b.col.text').text
+    puts "#{original_text}\t#{translated_text}"
 
   Card.create(original_text: original_text, translated_text: translated_text)
+  end
 end
